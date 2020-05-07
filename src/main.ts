266e8188aadf5c50afafa8d43812d7ccd56146a4
higuzero <higuzeroggw@gmail.com>
2020-05-08 03:18:50 +0900

    [mod] bootstrap の追加

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(IconsPlugin)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
--- src/views/MakeRoom.vue
+++ src/views/MakeRoom.vue
@@ -1,3 +1,49 @@
 <template>
-  <div class="makeroom"></div>
+  <div class="makeroom">
+    <h2>人狼ゲーム</h2>
+    <img class="icon_wolf" alt="icon_wolf logo" src="../assets/icon_wolf.png" />
+    <b-container fluid>
+      <b-row class="mt-3">
+        <b-col cols="8" offset="2">
+          <b-button-group>
+            <b-button variant="success" v-on:click="participate()">
+              <b-icon icon="clipboard" aria-hidden="true" />ルームに参加する
+            </b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+      <b-row class="mt-3">
+        <b-col cols="8" offset="2">
+          <b-button-group>
+            <b-button variant="success" v-on:click="organize()">
+              <b-icon icon="clipboard" aria-hidden="true" />ルームを作成する
+            </b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "MakeRoom",
+  data() {
+    return {
+      paticipatant: false
+    };
+  },
+  created() {
+    this.participate();
+  },
+  methods: {
+    participate: function() {
+      console.log("participate");
+      this.paticipatant = true;
+    },
+    organize: function() {
+      console.log("organize");
+    }
+  }
+};
+</script>
