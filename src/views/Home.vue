9e900be41e444a25f29b8a1e6fab65e2f99e2ae0
higuzero <higuzeroggw@gmail.com>
2020-05-09 09:08:43 +0900

    [mod] Home にルーム作成と参加画面を表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>人狼ゲーム</h2>
+    <img class="icon_wolf" alt="icon_wolf logo" src="../assets/icon_wolf.png" />
+    <b-container fluid>
+      <b-row class="mt-3">
+        <b-col cols="8" offset="2">
+          <b-button-group>
+            <b-button variant="success" v-on:click="participate()">
+              <b-icon icon="clipboard" aria-hidden="true" />ゲームに参加する
+            </b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+      <b-row class="mt-3">
+        <b-col cols="8" offset="2">
+          <b-button-group>
+            <b-button variant="success" v-on:click="organize()">
+              <b-icon icon="clipboard" aria-hidden="true" />ゲームを開催する
+            </b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+    </b-container>
+    <amplify-connect :mutation="createGameInfoMutation" @done="onCreatedFinished">
+      <template slot-scope="{ loading, mutate }">
+        <div v-if="loading">Loading...</div>
+        <!-- <div v-else-if="errors.length > 0">error</div> -->
+        <b-button variant="outline-primary" @click="mutate" class="mt-2">create</b-button>
+      </template>
+    </amplify-connect>
   </div>
 </template>
 
@@ -11,16 +38,70 @@
 ></script>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { API, graphqlOperation } from "aws-amplify";
 
+const getGameInfoQuery = /* GraphQL */ `
+  query GetGameInfo($id: ID!) {
+    getGameInfo(id: $id) {
+      id
+      type
+      owner
+      werewolf
+      people
+    }
+  }
+`;
+
+const CreateGameInfoMutation = `mutation CreateGameInfo(
+    $id: ID!,
+    $type: String!,
+    $owner: String!,
+    $werewolf: Int!,
+    $people: Int!
+    ){
+    createGameInfo(input: {     
+        id: $id,
+        type:$type,
+        owner:$owner,
+        werewolf:$werewolf,
+        people:$people
+  }){
+    id
+    type
+    owner
+    werewolf
+    people
+  }
+}
+`;
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      paticipatant: false,
+      roomid: "test",
+      type: "test",
+      owner: "test",
+      werewolf: 2,
+      people: 6
+    };
   },
   created() {
     this.initializeLiff();
+    this.participate();
+    this.checkRoomid();
+    this.getGameInfo();
+  },
+  computed: {
+    createGameInfoMutation() {
+      return this.$Amplify.graphqlOperation(CreateGameInfoMutation, {
+        id: this.roomid,
+        type: this.type,
+        owner: this.owner,
+        werewolf: this.werewolf,
+        people: this.people
+      });
+    }
   },
   methods: {
     initializeLiff: function() {
@@ -33,7 +114,36 @@ export default {
           this.$store.dispatch("setLoginData", idToken);
         }
       );
+    },
+    getGameInfo: async function() {
+      const room = await API.graphql(
+        graphqlOperation(getGameInfoQuery, { id: this.roomid })
+      );
+      console.log(room);
+    },
+    // 今度type をみてそれでroomId を決定する
+    checkRoomid: function() {
+      this.type = this.$store.state.loginData.type;
+      this.roomid = this.$store.state.loginData.groupId;
+      this.owner = this.$store.state.loginData.owner;
+    },
+    participate: function() {
+      console.log("participate");
+      this.paticipatant = true;
+    },
+    organize: function() {
+      console.log("organize");
+    },
+    onCreatedFinished: function() {
+      console.log("Finish Create");
     }
   }
 };
 </script>
+
+<style>
+.icon_wolf {
+  height: 100px;
+  width: 100px;
+}
+</style>
