75bccbb5c331f8d2a41dd2a0c4eb62e3c0572f51
higuzero <higuzeroggw@gmail.com>
2020-05-21 20:59:57 +0900

    [mod] 投票機能の追加、まだローカルのみ

--- src/views/DaytimeGame.vue
+++ src/views/DaytimeGame.vue
@@ -4,10 +4,15 @@
     <b-container fluid>
       <b-row>
         <b-col cols="10" offset="1">
-          <b-form-select v-model="fever_integer_selected" :options="nameList"/>
+          <b-form-select v-model="vote" :options="nameList"/>
         </b-col>
       </b-row>
     </b-container>
+    <b-button-group class="mt-4">
+      <b-button variant="success" @click="startVote()">
+        投票
+      </b-button>
+    </b-button-group>
     <b-container fluid>
       <b-row class="mt-3">
         <b-col cols="10" offset="1">
@@ -19,7 +24,9 @@
 </template>
 
 <script>
+import { API, graphqlOperation } from "aws-amplify";
 import DaytimeGameList from "@/components/DaytimeGameList.vue";
+import { updatePlayer } from "./../graphql/mutations";
 
 export default {
   name: "daytime",
@@ -29,11 +36,22 @@ export default {
   data() {
     return {
       players: this.$store.state.gameInfo.players,
-      nameList: ""
+      nameList: "",
+      vote: ""
     }
   },
   created() {
     this.nameList = this.players.map(player => player.userName);
+  },
+  methods: {
+    startVote: async function() {
+      console.log(this.vote);
+      this.$store.state.player.vote = this.vote;
+      const result = await API.graphql(
+        graphqlOperation(updatePlayer, { input: this.$store.state.player })
+      );
+      console.log(result);
+    }
   }
 };
 </script>
