96665a4a92b9e70462a6c68f01965f329cbf5010
higuuu <42379013+higuuu@users.noreply.github.com>
2021-12-31 01:11:42 +0900

    delete some info

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://7ah5cc554nclzaggv57thx7pre.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-xosvyyb5trgezoxrq2ynb54tb4"
+    "aws_appsync_apiKey": ""
 };
 
 
